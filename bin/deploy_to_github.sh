7d978b6d22892f0043b8dd080d9d70657cdbd977
杨海涛 <516330797@qq.com>
2022-11-13 20:54:50 +0800

    添加 build base，添加自动部署到 github 的脚本

---
+++ bin/deploy_to_github.sh
@@ -0,0 +1,10 @@
+#!/user/bin/env bash
+rm -rf dist
+npm run build
+cd dist
+git init
+git add .
+git commit -m deploy
+git remote add origin git@github.com:lLeahDizon/react-mangosteen-1-preview.git
+git push -f origin master:master
+cd -
