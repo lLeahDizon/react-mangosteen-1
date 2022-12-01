5ebe4267a88d8072204b5da43f672f9d1b0ee1fc
lLeahDizon <516330797@qq.com>
2022-12-01 20:27:25 +0800

    添加四个路由

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { NavLink, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { RedirectToWelcome1 } from './components/RedirectToWelcome1'
 
 const router = createBrowserRouter([
@@ -15,10 +15,22 @@ const router = createBrowserRouter([
         element: <Outlet/>,
         children: [
           { index: true, element: <div>空</div> },
-          { path: '1', element: <div>1</div> },
-          { path: '2', element: <div>2</div> },
-          { path: '3', element: <div>3</div> },
-          { path: '4', element: <div>4</div> },
+          {
+            path: '1',
+            element: <div>1 <NavLink to="/welcome/2">下一页</NavLink></div>,
+          },
+          {
+            path: '2',
+            element: <div>2 <NavLink to="/welcome/3">下一页</NavLink></div>,
+          },
+          {
+            path: '3',
+            element: <div>3 <NavLink to="/welcome/4">下一页</NavLink></div>,
+          },
+          {
+            path: '4',
+            element: <div>4 <NavLink to="/xxx">开始记账</NavLink></div>,
+          },
         ],
       },
     ],
