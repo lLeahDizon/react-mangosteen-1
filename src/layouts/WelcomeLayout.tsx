1f4339581bf07f4dbcf735da473410ae4a20e011
lLeahDizon <516330797@qq.com>
2022-12-18 16:55:27 +0800

    完善路由动画细节

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,27 +1,24 @@
 import { animated, useTransition } from '@react-spring/web'
 import { useLocation, useOutlet } from 'react-router-dom'
 import type { ReactNode } from 'react'
-import React from 'react'
+import React, { useRef } from 'react'
 
-const map: Record<string, ReactNode> = {}
 export const WelcomeLayout: React.FC = () => {
+  const map = useRef<Record<string, ReactNode>>({})
   const location = useLocation() // 获取当前地址栏属性
   const outlet = useOutlet()
-  map[location.pathname] = outlet
+  map.current[location.pathname] = outlet
   const transitions = useTransition(location.pathname, {
-    // 进入状态
-    from: { transform: 'translateX(100%)' },
-    // 稳定状态
+    from: { transform: location.pathname === '/welcome/1' ? 'translateX(0%)' : 'translateX(100%)' },
     enter: { transform: 'translateX(0%)' },
-    // 退出状态
     leave: { transform: 'translateX(-100%)' },
-    config: { duration: 1000 },
+    config: { duration: 300 },
   })
-  return transitions((style, pathname) => {
-    return <animated.div key={pathname} style={style}>
-      <div style={{ textAlign: 'center' }}>
-        {map[pathname]}
+  return transitions((style, pathname) =>
+    <animated.div key={pathname} style={style}>
+      <div>
+        {map.current[pathname]}
       </div>
-    </animated.div>
-  })
+    </animated.div>,
+  )
 }
